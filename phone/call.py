72d7835a0f1eabfaaf838d108e8cb7b6f56dd6c2
Edward Milkey <edward@milkey.org>
2019-01-20 17:56:34 -0600

    validate twilio request

--- phone/call.py
+++ phone/call.py
@@ -1,13 +1,25 @@
 import os
+import logging
 
-from flask import Blueprint
+from flask import Blueprint, request, abort
 from twilio.twiml.voice_response import VoiceResponse
+from twilio.request_validator import RequestValidator
 
 bp = Blueprint('call', __name__, url_prefix='/call')
 
 
-@bp.route('/forward')
+def validate_request() -> bool:
+    validator = RequestValidator(os.environ['TWILIO_AUTH_TOKEN'])
+    twilio_signature = request.headers.get('X-TWILIO-SIGNATURE', '')
+    return validator.validate(request.url, request.form, twilio_signature)
+
+
+@bp.route('/forward', methods=['POST'])
 def forward():
+    if os.environ.get('FLASK_ENV') != 'development':
+        if not validate_request():
+            logging.info('Request failed validation')
+            return abort(400)
     resp = VoiceResponse()
     resp.dial(os.environ['PHONE_FORWARD_NUMBER'])
     return str(resp)
--- tests/test_call.py
+++ tests/test_call.py
@@ -1,7 +1,16 @@
 import os
+from unittest import mock
 
 
 def test_forward(client):
-    response = client.get('/call/forward')  # <class 'flask.wrappers.Response'>
+    json_data = {}
+    response = client.post('/call/forward', json=json_data)  # <class 'flask.wrappers.Response'>
     assert '<Dial>{}</Dial>'.format(os.environ['PHONE_FORWARD_NUMBER']) in response.data.decode()
     assert response.status_code == 200
+
+
+def test_validate(client):
+    with mock.patch.dict('os.environ', {'FLASK_ENV': 'production'}):
+        json_data = {}
+        response = client.post('/call/forward', json=json_data)  # <class 'flask.wrappers.Response'>
+        assert response.status_code == 400
